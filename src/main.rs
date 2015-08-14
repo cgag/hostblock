e55f5297bc356a1e98fc44c5b20f3b31e099bb45
Curtis Gagliardi <curtis@curtis.io>
2015-08-14 16:33:32 -0700

    broken, wip on immutable state

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ use std::fs::File;
 use std::io::Read;
 use std::cmp::{min};
 
-struct State<'a>{
+// TODO(cgag): perhaps the rustbox instance should
+// live in here, and then write/write_inverted, render, etc
+// could be methods on State. 
+#[derive(Clone,Copy)]
+struct State<'a> {
     selected: usize,
-    lines: Vec<&'a str>,
+    domains: &'a Vec<&'a str>,
+}
+
+enum Movement { 
+    Top,
+    Bottom,
+    Up,
+    Down,
 }
 
 fn main() {
@@ -16,44 +27,53 @@ fn main() {
     let rustbox = RustBox::init(Default::default()).unwrap();
     let hosts_text = read_hosts();
 
-    let mut state = State { 
-                        selected: 0, 
-                        lines: hosts_text.lines().collect(),
-                    };
+    let init_state = State { 
+                    selected: 0, 
+                    domains: &hosts_text.lines().collect(),
+                };
 
     w_inv(&rustbox,0,0,"Press q to quit");
 
-    render(&rustbox, &state, &hosts_text);
+    render(&rustbox, &init_state);
     rustbox.present();
+
+    let mut state = init_state;
     loop {
-        match rustbox.poll_event(false) {
+        let newState = match rustbox.poll_event(false) {
             Ok(rustbox::Event::KeyEvent(key)) => {
                 match key {
                     Some(Key::Char('q')) => { break; },
                     Some(Key::Char('j')) => { 
-                        if state.selected == state.lines.len() - 1 {
-                            state.selected = 0
-                        } else {
-                            state.selected += 1;
-                        }
+                        // if state.selected == state.domains.len() - 1 {
+                        //     State { selected: 0
+                        //           , domains: state.domains }
+                        // } else {
+                            State { selected: state.selected + 1
+                                  , domains: state.domains }
+                        // }
                     },
                     Some(Key::Char('k')) => { 
                         if state.selected == 0 {
-                            state.selected = state.lines.len() - 1;
+                            State { selected: state.domains.len() - 1
+                                  , domains: state.domains }
                         } else {
-                            state.selected -= 1;
+                            State { selected: state.selected - 1 
+                                  , domains: state.domains }
                         }
                     },
                     Some(Key::Char('J')) => { 
-                        state.selected = state.lines.len() - 1 
+                        State { selected: state.domains.len() - 1 
+                              , domains: state.domains }
                     },
-                    Some(Key::Char('K')) => { state.selected = 0 },
-                    _ => {}
+                    Some(Key::Char('K')) => { moveSel(state, Movement::Top) }
+                    _ => { state }
                 }
             }
-            _ => {}
-        }
-        render(&rustbox, &state, &hosts_text);
+            _ => { panic!("failed to read event, no idea why this would happen") }
+        };
+
+        state = newState;
+        render(&rustbox, &state);
         rustbox.present();
     }
 }
@@ -74,6 +94,16 @@ fn read_hosts() -> String {
     s
 }
 
+
+fn moveSel(state: &State, movement: Movement) -> State {
+    match movement {
+        Top => { State { selection: 0, domains: state.domains } }
+        Bottom => {}
+        Up => {}
+        Down => {}
+    }
+}
+
 // TODO(cgag): make these methods on rustbox
 fn w(b: &RustBox, x: usize, y: usize, text: &str) {
     b.print(x,y,rustbox::RB_BOLD, Color::White, Color::Black, text);
@@ -83,8 +113,8 @@ fn w_inv(b: &RustBox, x: usize, y: usize, text: &str) {
     b.print(x,y,rustbox::RB_BOLD, Color::Black, Color::White, text);
 }
 
-fn render(b: &RustBox, state: &State, hosts_text: &str) {
-    for (i, line) in hosts_text.lines().enumerate() {
+fn render(b: &RustBox, state: &State) {
+    for (i, line) in state.domains.iter().enumerate() {
         if i == state.selected {
             w_inv(b, 0, i, line);
         } else {
