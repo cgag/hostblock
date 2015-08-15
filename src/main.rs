6209a9060785751b43c149f3a7018abb85fc3f44
Curtis Gagliardi <curtis@curtis.io>
2015-08-15 14:09:19 -0700

    immutalbe state, handle_event, only read hostblock

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::default::Default;
 use rustbox::{RustBox,Color,Key};
 use std::fs::File;
 use std::io::Read;
-use std::cmp::{min};
 
 // TODO(cgag): perhaps the rustbox instance should
 // live in here, and then write/write_inverted, render, etc
@@ -12,7 +11,7 @@ use std::cmp::{min};
 #[derive(Clone,Copy)]
 struct State<'a> {
     selected: usize,
-    domains: &'a Vec<&'a str>,
+    domains: &'a Vec<String>,
 }
 
 enum Movement { 
@@ -22,15 +21,19 @@ enum Movement {
     Down,
 }
 
+enum DomainStatus { Blocked, Unblocked }
+struct Domain {
+    domain: String,
+    blocked: DomainStatus,
+}
+
 fn main() {
-
     let rustbox = RustBox::init(Default::default()).unwrap();
-    let hosts_text = read_hosts();
+    let domains = read_hosts();
 
-    let init_state = State { 
-                    selected: 0, 
-                    domains: &hosts_text.lines().collect(),
-                };
+    let init_state = State { selected: 0
+                           , domains:  &domains
+                           };
 
     w_inv(&rustbox,0,0,"Press q to quit");
 
@@ -39,46 +42,16 @@ fn main() {
 
     let mut state = init_state;
     loop {
-        let newState = match rustbox.poll_event(false) {
-            Ok(rustbox::Event::KeyEvent(key)) => {
-                match key {
-                    Some(Key::Char('q')) => { break; },
-                    Some(Key::Char('j')) => { 
-                        // if state.selected == state.domains.len() - 1 {
-                        //     State { selected: 0
-                        //           , domains: state.domains }
-                        // } else {
-                            State { selected: state.selected + 1
-                                  , domains: state.domains }
-                        // }
-                    },
-                    Some(Key::Char('k')) => { 
-                        if state.selected == 0 {
-                            State { selected: state.domains.len() - 1
-                                  , domains: state.domains }
-                        } else {
-                            State { selected: state.selected - 1 
-                                  , domains: state.domains }
-                        }
-                    },
-                    Some(Key::Char('J')) => { 
-                        State { selected: state.domains.len() - 1 
-                              , domains: state.domains }
-                    },
-                    Some(Key::Char('K')) => { moveSel(state, Movement::Top) }
-                    _ => { state }
-                }
-            }
-            _ => { panic!("failed to read event, no idea why this would happen") }
-        };
-
-        state = newState;
+        let (quit, new_state) = 
+            handle_event(rustbox.poll_event(false).ok().expect("fuck"), state);
+        if quit { break }
+        state = new_state;
         render(&rustbox, &state);
         rustbox.present();
     }
 }
 
-fn read_hosts() -> String {
+fn read_hosts() -> Vec<String> {
     let mut hosts_file: File;
 
     let x = File::open("/etc/hosts");
@@ -91,20 +64,67 @@ fn read_hosts() -> String {
     
     let mut s = String::new();
     hosts_file.read_to_string(&mut s).unwrap();
-    s
+    s.lines()
+        .take_while(|s| !s.starts_with("### End HostBlock"))
+        .skip_while(|s| !s.starts_with("### HostBlock"))
+        .skip(1) // drop the ### HostBlock line
+        .map(|line| line.to_string())
+        .collect::<Vec<String>>()
 }
 
+fn handle_event(event: rustbox::Event, state: State) -> (bool, State) {
+    let mut should_quit = false;
 
-fn moveSel(state: &State, movement: Movement) -> State {
+    // TODO(cgag): avoid all these default cases returning state somehow?
+    let new_state = match event {
+        rustbox::Event::KeyEvent(mkey) => {
+            match mkey {
+                Some(key) => match key {
+                    Key::Char('q') => { should_quit = true; state },
+                    Key::Char('j') => { move_sel(state, Movement::Down)   },
+                    Key::Char('k') => { move_sel(state, Movement::Up)     },
+                    Key::Char('J') => { move_sel(state, Movement::Bottom) },
+                    Key::Char('K') => { move_sel(state, Movement::Top)    }
+                    _  => { state }
+                },
+                _ => { state }
+            }
+        }
+        _ => { state } 
+    };
+
+    (should_quit, new_state)
+}
+
+fn move_sel(state: State, movement: Movement) -> State {
     match movement {
-        Top => { State { selection: 0, domains: state.domains } }
-        Bottom => {}
-        Up => {}
-        Down => {}
+        Movement::Top => { State { selected: 0, domains: state.domains } }
+        Movement::Bottom => { 
+            State { selected: state.domains.len()
+                  , domains: state.domains }
+        }
+        Movement::Up => {
+            if state.selected == 0 {
+                State { selected: state.domains.len() - 1
+                      , domains: state.domains }
+            } else {
+                State { selected: state.selected - 1 
+                      , domains: state.domains }
+            }
+        }
+        Movement::Down => {
+            if state.selected == state.domains.len() - 1 {
+                State { selected: 0
+                      , domains: state.domains }
+            } else {
+                State { selected: state.selected + 1
+                      , domains: state.domains }
+            }
+        }
     }
 }
 
-// TODO(cgag): make these methods on rustbox
+// TODO(cgag): make these methods on rustbox?
 fn w(b: &RustBox, x: usize, y: usize, text: &str) {
     b.print(x,y,rustbox::RB_BOLD, Color::White, Color::Black, text);
 }
@@ -115,11 +135,12 @@ fn w_inv(b: &RustBox, x: usize, y: usize, text: &str) {
 
 fn render(b: &RustBox, state: &State) {
     for (i, line) in state.domains.iter().enumerate() {
+        let mut s = String::from("[ ]");
+        s.extend(line.chars());
         if i == state.selected {
-            w_inv(b, 0, i, line);
+            w_inv(b, 0, i, &s);
         } else {
-            w(b, 0, i, line);
+            w(b, 0, i, &s);
         }
     }
 }
-// fn wInv
